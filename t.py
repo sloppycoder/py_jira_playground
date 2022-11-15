5f97a26845b9f22f41308e3f4fd6b9df1fb977cb
Li Lin <guru.lin@gmail.com>
2022-11-15 14:49:59 +0800

    inovking multiple calls for pagination

--- t.py
+++ t.py
@@ -1,12 +1,17 @@
+import logging
 import os
 import pprint
+import sys
 
 from dotenv import load_dotenv
 from jira import JIRA
 
-load_dotenv()
+logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 
-names_map = {}
+custom_fields = {}
+
+query1 = 'created >= -30w AND project = EIPP AND issuetype in ("BE DEV Task", "FE AND DEV Task", "FE IOS DEV Task", "FE WEB DEV Task") AND status in (BE-AD-FOR-REVIEW, BE-AD-IN-PROGRESS, BE-AD-TO-DO, BE-DEV-FOR-REVIEW, BE-DEV-IN-PROGRESS, BE-DEV-TO-DO, Blocked, FE-AD-FOR-REVIEW, FE-AD-IN-PROGRESS, FE-AD-TO-DO, FE-DEV-FOR-REVIEW, FE-DEV-IN-PROGRESS, FE-DEV-TO-DO) AND Sprint = 2138 '
+query2 = 'created >= -30w AND project = EIPP AND key in ("EIPP-245")'
 
 
 def jira_connect() -> JIRA:
@@ -17,29 +22,40 @@ def jira_connect() -> JIRA:
 
 
 def issue_to_map(jira: JIRA, issue):
-    global names_map
-    if names_map == {}:
-        names_map = {field["name"]: field["id"] for field in jira.fields()}
+    global custom_fields
+    if custom_fields == {}:
+        custom_fields = {field["name"]: field["id"] for field in jira.fields()}
 
     return {
-        "Key": issue.fields.project.key,
-        "Type": issue.fields.issuetype.name,
-        "Status": issue.fields.status.name,
-        "Summary": issue.fields.summary,
-        "Labels": issue.fields.labels,
-        "Feature": getattr(issue.fields, names_map["BN Feature Grouping"]),
+        "key": issue.fields.project.key,
+        "type": issue.fields.issuetype.name,
+        "status": issue.fields.status.name,
+        "summary": issue.fields.summary,
+        "labels": issue.fields.labels,
+        "feature": getattr(issue.fields, custom_fields["BN Feature Grouping"]),
     }
 
 
-def get_issue_list(jira: JIRA):
-    jql = 'created >= -30w AND project = EIPP AND issuetype in ("BE DEV Task", "FE AND DEV Task", "FE IOS DEV Task", "FE WEB DEV Task") AND status in (BE-AD-FOR-REVIEW, BE-AD-IN-PROGRESS, BE-AD-TO-DO, BE-DEV-FOR-REVIEW, BE-DEV-IN-PROGRESS, BE-DEV-TO-DO, Blocked, FE-AD-FOR-REVIEW, FE-AD-IN-PROGRESS, FE-AD-TO-DO, FE-DEV-FOR-REVIEW, FE-DEV-IN-PROGRESS, FE-DEV-TO-DO) AND Sprint = 2138 '
-    # jql = 'created >= -30w AND project = EIPP AND key in ("EIPP-245")'
-    result = jira.search_issues(jql, maxResults=200)
-    print(f"total issues: {len(result)}")
-    for item in result:
-        pprint.pprint(issue_to_map(jira, item))
+def search_issue(jira: JIRA, jql: str, max_results=1000, page_size=100):
+    for offset in range(0, max_results, page_size):
+        logging.debug(f"calling search_issues with maxResults={page_size}, startAt={offset}")
+        issues = jira.search_issues(jql, maxResults=page_size, startAt=offset)
+        for issue in issues:
+            yield issue
+        if len(issues) < page_size:
+            logging.debug("issues exhausted, break")
+            break
+
+
+def main():
+    jira = jira_connect()
+    count = 0
+    for issue in search_issue(jira, query1):
+        count += 1
+        pprint.pprint(issue)
+    print(f"\ntotal issues: {count}")
 
 
 if __name__ == "__main__":
-    jira = jira_connect()
-    get_issue_list(jira)
+    load_dotenv()
+    main()
