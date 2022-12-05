e91a15293996a70954db6d07e680b52067ed108c
Li Lin <guru.lin@gmail.com>
2022-12-05 11:02:46 +0800

    add gtilab script

---
+++ gl.py
@@ -0,0 +1,31 @@
+import os
+from configparser import ConfigParser
+from fnmatch import fnmatch
+
+import requests
+from gitlab import Gitlab, GitlabAuthenticationError, GitlabGetError
+
+requests.packages.urllib3.disable_warnings()
+
+
+def enumerate_gitlab_projects(url, group, xfilter, ssl_verify=False):
+    token = os.environ.get("GITLAB_TOKEN")
+
+    try:
+        gl = Gitlab(url, private_token=token, ssl_verify=ssl_verify)
+        projects = gl.groups.get(group).projects.list(include_subgroups=True, as_list=False)
+        return [proj for proj in projects if fnmatch(proj.path_with_namespace, xfilter)]
+    except GitlabAuthenticationError:
+        print(f"authentication error {url}, {token}, {ssl_verify}")
+    except GitlabGetError as e:
+        print(f"gitlab search {group} error {type(e)} => {e}")
+    return []
+
+
+def print_all_repos():
+    for repo in enumerate_gitlab_projects("https://gitlab.com", "mobilityaccelerator", "*"):
+        print(repo.ssh_url_to_repo)
+
+
+if __name__ == "__main__":
+    print_all_repos()
