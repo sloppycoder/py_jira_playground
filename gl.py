e826139008a369a69f940cf129ccd156fd191559
Li Lin <guru.lin@gmail.com>
2022-12-09 16:49:18 +0800

    refactor parameters and filters

--- gl.py
+++ gl.py
@@ -1,9 +1,10 @@
 import os
 import platform
+import sys
 from fnmatch import fnmatch
 
 import requests
-from dotenv import load_dotenv
+from dotenv import dotenv_values
 from gitlab import Gitlab, GitlabAuthenticationError, GitlabGetError
 
 requests.packages.urllib3.disable_warnings()
@@ -13,13 +14,10 @@ requests.packages.urllib3.disable_warnings()
 # platform.system = MagicMock(return_value="Windows")
 
 
-def enumerate_gitlab_projects(url, group, xfilter, ssl_verify=False):
-    token = os.environ.get("GITLAB_TOKEN")
-
+def enumerate_gitlab_projects(url, group, token, ssl_verify=False):
     try:
         gl = Gitlab(url, private_token=token, ssl_verify=ssl_verify)
-        projects = gl.groups.get(group).projects.list(include_subgroups=True, as_list=False)
-        return [proj for proj in projects if fnmatch(proj.path_with_namespace, xfilter)]
+        return gl.groups.get(group).projects.list(include_subgroups=True, as_list=False)
     except GitlabAuthenticationError:
         print(f"authentication error {url}, {token}, {ssl_verify}")
     except GitlabGetError as e:
@@ -31,8 +29,7 @@ def mkdir_cmd():
     return "mkdir -Force " if platform.system() == "Windows" else "mkdir -p"
 
 
-def clone_script(parent_path, repo, clone_url):
-    base_path = os.environ.get("LOCAL_BASE_PATH")
+def clone_script(base_path, parent_path, repo, clone_url):
     output_path = f"{base_path}/{parent_path}"
     print(f"# {clone_url}")
 
@@ -51,14 +48,33 @@ def clone_script(parent_path, repo, clone_url):
         print("\n")
 
 
-def print_all_repos():
-    # url, group, xfilter = "https://gitlab.com", "mobilityaccelerator", "*"
-    url, group, xfilter = "https://gitlab-channel.devcloud.krungthai/", "EIPP", "eipp/backend/*"
-    for repo in enumerate_gitlab_projects(url, group, xfilter):
-        parent_path = "/".join(repo.path_with_namespace.split("/")[:-1])
-        clone_script(parent_path, repo.name, repo.ssh_url_to_repo)
+def print_clone_all_script(url, group, token, local_base_path, xfilter="*", strip_prefix=""):
+    strip_prefix = group + "/" + strip_prefix
+
+    for repo in enumerate_gitlab_projects(url, group, token):
+        if not fnmatch(repo.path_with_namespace, xfilter):
+            continue
+
+        repo_path = repo.path_with_namespace
+        if repo_path.find(strip_prefix) == 0:
+            parent_path = repo_path[len(strip_prefix) + 1 :]
+        else:
+            parent_path = repo_path
+
+        clone_script(local_base_path, parent_path, repo.name, repo.ssh_url_to_repo)
 
 
 if __name__ == "__main__":
-    load_dotenv()
-    print_all_repos()
+    if len(sys.argv) <= 1:
+        conf = dotenv_values("gl.env")
+    else:
+        conf = dotenv_values(sys.argv[1])
+
+    print_clone_all_script(
+        conf["GITLAB_URL"],
+        conf["GITLAB_GROUP"],
+        conf["GITLAB_TOKEN"],
+        conf["LOCAL_BASE_PATH"],
+        conf["GITLAB_PROJECT_FILTER"],
+        conf["STRIP_PREFIX"],
+    )
